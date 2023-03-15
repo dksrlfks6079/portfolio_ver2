c0f1fee4ba0353f63bbcf6811fe7445130c42c35
Akr <dksrlfks6079@naver.com>
2023-03-15 11:03:04 +0900

    port

--- js/common.js
+++ js/common.js
@@ -8,10 +8,8 @@ $(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
             $(".top").addClass("on")
-            // $(".inner").addClass("on")
         } else {
             $(".top").removeClass("on")
-            // $(".inner").removeClass("on")
         }
     })
 })
