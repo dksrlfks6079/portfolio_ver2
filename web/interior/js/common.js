6419a199e18b416760c85b73ca8ab0f774dea9f6
Akr <dksrlfks6079@naver.com>
2023-03-20 11:33:07 +0900

    port

---
+++ web/interior/js/common.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".nav").slick({
+        // slide: "li",
+        slidesToShow: 3,
+        // centerMode: true,
+        arrows: false,
+        infinite: false,
+        variableWidth: true,
+    })
+})
